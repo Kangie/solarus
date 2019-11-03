dd7fa00b61f7386365983277f999ef1972590728
Hugo Hromic <hhromic@gmail.com>
2019-11-03 19:22:47 +0000

    Modernize Solarus(GUI) CMake find modules
    * Can use `SOLARUS_DIR` and `SOLARUS_GUI_DIR` (optional) environment and
      CMake variables to provide a custom installation directory
    * Extract Solarus version number from config header
    * Make sure that the found Solarus version matches the project version

--- cmake/modules/FindSolarusGui.cmake
+++ cmake/modules/FindSolarusGui.cmake
@@ -1,21 +1,56 @@
-# - Find Solarus
+# - Find Solarus GUI
 # Find the Solarus GUI includes and library.
 #
-#  SOLARUS_GUI_INCLUDE_DIRS - where to find solarus GUI headers.
-#  SOLARUS_GUI_LIBRARIES    - List of libraries when using libsolarus-gui.
-#  SOLARUS_GUI_FOUND        - True if Solarus GUI was found.
-find_path(SOLARUS_GUI_INCLUDE_DIR solarus/gui/main_window.h)
+#  SOLARUS_GUI_INCLUDE_DIRS   - where to find solarus GUI headers.
+#  SOLARUS_GUI_LIBRARIES      - List of libraries when using libsolarus-gui.
+#
+# A custom installation path to search can be provided using
+# the SOLARUS_GUI_DIR environment or CMake variables. If not found,
+# the SOLARUS_DIR environment or CMake variables are used as fallback.
 
-find_library(SOLARUS_GUI_LIBRARY solarus-gui)
+# Try to find the Solarus GUI include directory.
+find_path(SOLARUS_GUI_INCLUDE_DIR
+  NAMES
+    solarus/gui/main_window.h
+  HINTS
+    ${SOLARUS_GUI_DIR}
+    $ENV{SOLARUS_GUI_DIR}
+    ${SOLARUS_DIR}
+    $ENV{SOLARUS_DIR}
+  PATHS
+    /sw # Fink
+    /opt/local # DarwinPorts
+    /opt/csw # Blastwave
+    /opt
+  PATH_SUFFIXES
+    include
+)
 
+# Try to find the Solarus GUI library.
+find_library(SOLARUS_GUI_LIBRARY
+  NAMES
+    solarus-gui
+  HINTS
+    ${SOLARUS_GUI_DIR}
+    $ENV{SOLARUS_GUI_DIR}
+    ${SOLARUS_DIR}
+    $ENV{SOLARUS_DIR}
+  PATHS
+    /sw # Fink
+    /opt/local # DarwinPorts
+    /opt/csw # Blastwave
+    /opt
+  PATH_SUFFIXES
+    lib
+    lib64
+)
+
+# Check if include and library directories were found.
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(SOLARUS_GUI
-    FOUND_VAR SOLARUS_GUI_FOUND
-    REQUIRED_VARS
-        SOLARUS_GUI_INCLUDE_DIR
-        SOLARUS_GUI_LIBRARY)
-
-if(SOLARUS_GUI_FOUND)
-    set(SOLARUS_GUI_INCLUDE_DIRS ${SOLARUS_GUI_INCLUDE_DIR})
-    set(SOLARUS_GUI_LIBRARIES ${SOLARUS_GUI_LIBRARY})
-endif()
+find_package_handle_standard_args(SolarusGui
+  REQUIRED_VARS
+    SOLARUS_GUI_INCLUDE_DIR
+    SOLARUS_GUI_LIBRARY
+)
+set(SOLARUS_GUI_INCLUDE_DIRS ${SOLARUS_GUI_INCLUDE_DIR})
+set(SOLARUS_GUI_LIBRARIES ${SOLARUS_GUI_LIBRARY})
