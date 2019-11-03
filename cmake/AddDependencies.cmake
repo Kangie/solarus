d81a2778356eef61ee782c9dcbf639733c37c846
Hugo Hromic <hhromic@gmail.com>
2019-11-03 19:16:35 +0000

    Require minimum version numbers for dependencies
    * Minimum Qt version required is `5.7`
    * Minimum SDL2 version required is `2.0.6`
    * Exact Lua (non-JIT) version required is `5.1`

--- cmake/AddDependencies.cmake
+++ cmake/AddDependencies.cmake
@@ -4,12 +4,12 @@ set(CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH}" "${CMAKE_SOURCE_DIR}/cmake/modules/
 # Wheither LuaJIT should be used instead of vanilla Lua.
 option(SOLARUS_USE_LUAJIT "Use LuaJIT instead of default Lua (recommended)" ON)
 
-find_package(Qt5Core REQUIRED)
-find_package(Qt5Widgets REQUIRED)
-find_package(Qt5LinguistTools REQUIRED)
+find_package(Qt5Core "5.7" REQUIRED)
+find_package(Qt5Widgets "5.7" REQUIRED)
+find_package(Qt5LinguistTools "5.7" REQUIRED)
 find_package(Solarus REQUIRED)
 find_package(SolarusGui REQUIRED)
-find_package(SDL2 REQUIRED)
+find_package(SDL2 "2.0.6" REQUIRED)
 find_package(SDL2_image REQUIRED)
 find_package(SDL2_ttf REQUIRED)
 find_package(OpenAL REQUIRED)
@@ -20,7 +20,7 @@ find_package(PhysFS REQUIRED)
 if(SOLARUS_USE_LUAJIT)
   find_package(LuaJit REQUIRED)
 else()
-  find_package(Lua51 REQUIRED)
+  find_package(Lua51 "5.1" EXACT REQUIRED)
 endif()
 
 # Explicit link to libdl is needed for Lua on some systems.
