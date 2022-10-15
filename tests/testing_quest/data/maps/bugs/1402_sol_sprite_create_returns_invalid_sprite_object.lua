4c00a95612a121b1ae6b9e294c8d24c1ddf8857e
Cluedrew Kenfar Ink <cluedrew.of.ink@gmail.com>
2022-10-15 18:37:00 -0400

    I hope the new successful sprite create assertion is never useful, but it is there just in case.

--- tests/testing_quest/data/maps/bugs/1402_sol_sprite_create_returns_invalid_sprite_object.lua
+++ tests/testing_quest/data/maps/bugs/1402_sol_sprite_create_returns_invalid_sprite_object.lua
@@ -2,7 +2,11 @@ local map = ...
 
 function map:on_started()
   local sprite = sol.sprite.create"imaginary/bad-id"
-  assert_equal(nil, sprite, "Sprite should be nil.")
+  assert_equal(sprite, nil, "Sprite should be nil.")
+
+  sprite = sol.sprite.create"entities/arrow"
+  assert_equal(sol.main.get_type(sprite), "sprite",
+               "Sprite should be of type 'sprite'.")
 
   sol.main.exit()
 end
