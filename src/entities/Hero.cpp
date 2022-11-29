21b7044a6b282f926532f73ee539f25db779d39d
Merlin Duval <merlin.duval@imt-atlantique.net>
2022-11-29 18:15:55 +0100

    fix issue 1554

--- src/entities/Hero.cpp
+++ src/entities/Hero.cpp
@@ -2347,7 +2347,7 @@ void Hero::update_invincibility() {
 
   if (is_invincible() &&
       end_invincible_date != 0 &&
-      System::now_ms() >= end_invincible_date) {
+      System::now_ms() >= end_invincible_date && get_game().is_paused()) {
     set_invincible(false, 0);
   }
 }
