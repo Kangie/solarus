4d8b9799e157c75d8e5598285746b409c6e1252a
Christopho <christopho@solarus-games.org>
2023-11-12 13:00:21 +0100

    Add unit test

---
+++ tests/testing_quest/data/maps/bugs/1609_crash_save_lua_keyword.lua
@@ -0,0 +1,9 @@
+local map = ...
+local game = map:get_game()
+
+
+function map:on_opening_transition_finished()
+  local success, error_message = game:set_value("function", true)
+  assert_equal(success, false)
+  sol.main.exit()
+end
