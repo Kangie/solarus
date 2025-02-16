Kangie/solarus

commit 1f1d71d280752e60604abc86167c7dd3cdf65eb7

Fix map:draw_visual() not using map coordinates
Closes #1675.

src/core/Map.cpp
@@ -596,11 +596,12 @@ void Map::draw() {
     get_entities().draw(*camera);
 
     // foreground
-    camera->reset_view();
     //draw_foreground(camera_surface);
 
     // Lua
-    get_lua_context().map_on_draw(*this, camera_surface); //TODO check for coordinates problem
+    get_lua_context().map_on_draw(*this, camera_surface); //TODO check for coordinates
+
+    camera->reset_view();
   }
 }
 

doc/en/lua_api_map.dox
@@ -1842,17 +1842,19 @@ Called at each cycle of the main loop while this map is the current one.
 \remark As this function is called at each cycle, it is recommended to use other
 solutions when possible, like \ref lua_api_timer "timers" and other events.
 
-\subsection lua_api_map_on_draw map:on_draw(dst_surface)
+\subsection lua_api_map_on_draw map:on_draw(camera_surface)
 
 Called when the map has just been redrawn by the engine.
 
-The engine has already drawn the map, but not the
+The engine has already drawn the map contents, but not the
 \ref lua_api_menu "menus" of this map if any.
 Use this event if you want to draw some additional content on the map
 before the menus, for example an overlay.
-- \c dst_surface (\ref lua_api_surface "surface"): The surface where the map
-  is drawn. This surface represents the visible part of the screen, not the
-  whole map.
+- \c camera_surface (\ref lua_api_surface "surface"): The surface of the camera
+  being drawn.
+  This surface represents the visible part of the camera, not the whole map.
+
+\remark If the map has multiple cameras, this event is called for every camera.
 
 \subsection lua_api_map_on_suspended map:on_suspended(suspended)
 

include/solarus/entities/Entities.h
@@ -230,11 +230,8 @@ class SOLARUS_API Entities {
         tiles_in_animated_regions;                  /**< For each layer, animated tiles and tiles overlapping them. */
 
     // dynamic entities
-    /*HeroPtr hero;  */                                   /**< The hero, also stored in Game because
-                                                     * it is kept when changing maps. */
     std::vector<HeroPtr> heroes;
 
-    //CameraPtr camera;                               /**< The visible area of the map. */
     Cameras cameras;                                /**< The visibles area of the map. */
 
     std::map<std::string, EntityPtr>
@@ -283,7 +280,7 @@ inline Ground Entities::get_tile_ground(int layer, int x, int y) const {
  * \return The camera, or nullptr if there is no camera.
  */
 inline const CameraPtr Entities::get_camera() const {
-  if(cameras.size()) {
+  if (!cameras.empty()) {
     return cameras.front();
   } else {
     return nullptr;

include/solarus/graphics/Drawable.h
@@ -161,7 +161,7 @@ class Drawable: public ExportableToLua {
     uint8_t opacity = 255;        /**< Opacity of this drawable object */
     double rotation = 0;          /**< Rotation of the object around transform_origin*/
     Scale scale;                  /**< Scale of the object around transform_origin*/
-    Point transformation_origin;       /**< pivot for the transformations (rot,scale) of the object*/
+    Point transformation_origin;  /**< Pivot for the transformations (rot,scale) of the object */
     Color color_mod = Color::white;
 };
 

tests/include/tools/TestEnvironment.h
@@ -17,7 +17,6 @@
 #ifndef SOLARUS_TEST_ENVIRONMENT_H
 #define SOLARUS_TEST_ENVIRONMENT_H
 
-#include "solarus/core/Common.h"
 #include "solarus/core/Arguments.h"
 #include "solarus/core/MainLoop.h"
 #include "solarus/core/Point.h"

tests/testing_quest/data/project_db.dat
@@ -249,6 +249,7 @@ tileset{ id = "overworld", description = "Overworld" }
 
 sprite{ id = "16x16", description = "16x16 sprite" }
 sprite{ id = "16x24", description = "16x24 sprite" }
+sprite{ id = "1x1", description = "1x1 sprite" }
 sprite{ id = "blocks/block_brown", description = "Block (brown)" }
 sprite{ id = "chests/chest_brown", description = "Chest (brown)" }
 sprite{ id = "destructibles/vase", description = "Vase" }
@@ -1073,6 +1074,7 @@ file{ path = "sprites/16x16.dat", author = "Christopho", license = "CC BY-SA 4.0
 file{ path = "sprites/16x16.png", author = "Christopho", license = "CC BY-SA 4.0" }
 file{ path = "sprites/16x24.dat", author = "Christopho", license = "CC BY-SA 4.0" }
 file{ path = "sprites/16x24.png", author = "Christopho", license = "CC BY-SA 4.0" }
+file{ path = "sprites/1x1.dat", author = "Christopho", license = "CC BY-SA 4.0" }
 file{ path = "sprites/blocks/block.png", author = "Diarandor", license = "CC BY-SA 4.0" }
 file{ path = "sprites/blocks/block_brown.dat", license = "CC BY-SA 4.0" }
 file{ path = "sprites/chests/chest.png", author = "Diarandor", license = "CC BY-SA 4.0" }

tests/testing_quest/data/sprites/1x1.dat
@@ -1,0 +1,7 @@
+animation{
+  name = "normal",
+  src_image = "1x1.png",
+  directions = {
+    { x = 0, y = 0, frame_width = 1, frame_height = 1, origin_x = 0, origin_y = -2 },
+  },
+}

commit c0a7e4ba6691ab80b0c99d5bb4a8433b32036d3c

Fix shaders broken by 1f1d71d28

include/solarus/entities/Camera.h
@@ -91,14 +91,11 @@ class Camera : public Entity {
     void set_subpixel_offset(const glm::vec2& offset);
     Point get_position_on_screen(Scale px_scale) const;
 
-
     const SurfacePtr& get_surface() const;
 
     Point get_position_on_screen() const;
     void set_position_on_screen(const Point& position_on_screen);
     Point get_position_to_track(const Point& tracked_xy) const;
-
-
 
     void start_tracking(const EntityPtr& entity);
     void start_manual();
@@ -113,8 +110,8 @@ class Camera : public Entity {
 
     void reset_view();
     void apply_view();
+    bool is_view_applied() const;
 
-    void set_surface_mode();
     void notify_window_size_changed(const Size& new_size);
 
     Rectangle get_viewport_rectangle() const;
@@ -148,7 +145,8 @@ private:
     float rotation = 0.f;         /**< Rotation of this camera */
     glm::vec2 position_offset
     = {0.f,0.f};    /**< Small offset in position to compensate for discretization after zoom */
-    glm::vec2 subpixel_offset;       /**< Speed of subpixel move */
+    glm::vec2 subpixel_offset;    /**< Speed of subpixel move */
+    bool view_applied = false;    /**< Whether the view is set */
 };
 
 }

include/solarus/graphics/View.h
@@ -59,6 +59,7 @@ public:
 
   void set_viewport(const FRectangle& viewport);
   const FRectangle& get_viewport() const;
+
 private:
   inline void invalidate() {
     transform_dirty = true;
@@ -66,7 +67,7 @@ private:
   }
   glm::vec2 center = glm::vec2(0.f);        /**< Center at which the view looks */
   //glm::vec2 size;                         /**< Size the view covers */
-  glm::vec2 scale = glm::vec2(1.f);          /**< Scaling of the view */
+  glm::vec2 scale = glm::vec2(1.f);         /**< Scaling of the view */
   float rotation = 0;                       /**< Rotation of the view around the center */
   FRectangle viewport;                      /**< Viewport of this view, in fraction of the */
   mutable glm::mat4 transform;              /**< Cached transform from the viewed space to target space */

src/core/Map.cpp
@@ -582,8 +582,8 @@ void Map::draw() {
     return;
   }
 
-  for(const CameraPtr& camera : get_entities().get_cameras()) {
-    if(camera->is_being_removed()){
+  for (const CameraPtr& camera : get_entities().get_cameras()) {
+    if (camera->is_being_removed()) {
         continue;
     }
     const SurfacePtr& camera_surface = camera->get_surface();
@@ -596,12 +596,11 @@ void Map::draw() {
     get_entities().draw(*camera);
 
     // foreground
-    //draw_foreground(camera_surface);
+    camera->reset_view();
+    // draw_foreground(camera_surface);
 
     // Lua
-    get_lua_context().map_on_draw(*this, camera_surface); //TODO check for coordinates
-
-    camera->reset_view();
+    get_lua_context().map_on_draw(*this, camera_surface); // TODO check for coordinates
   }
 }
 
@@ -678,7 +677,23 @@ void Map::draw_foreground(const SurfacePtr& dst_surface) {
  */
 void Map::draw_visual(Drawable& drawable, const Point &xy) {
 
-  draw_visual(drawable, xy.x, xy.y);
+  const CameraPtr& camera = get_camera();
+  if (camera == nullptr) {
+    return;
+  }
+
+  Point dst = xy;
+  if (!camera->is_view_applied()) {
+    // When called from map:on_draw(), drawing onto the surface
+    // expects coordinates relative the camera.
+    // When called from entity:on_pre_draw(), it expects
+    // coordinates relative to the map.
+    // See how Map::draw() above calls apply_view()/reset_view().
+//    dst -= camera->get_xy();
+  }
+
+  const SurfacePtr& camera_surface = camera->get_surface();
+  drawable.draw(camera_surface, dst);
 }
 
 /**
@@ -688,18 +703,7 @@ void Map::draw_visual(Drawable& drawable, const Point &xy) {
  * \param y Y coordinate of the drawable's origin point in the map.
  */
 void Map::draw_visual(Drawable& drawable, int x, int y) {
-
-  // The position is given in the map coordinate system:
-  // convert it to the visible surface coordinate system.
-  const CameraPtr& camera = get_camera();
-  if (camera == nullptr) {
-    return;
-  }
-  const SurfacePtr& camera_surface = camera->get_surface();
-  drawable.draw(camera_surface,
-      x,
-      y
-  );
+  draw_visual(drawable, {x, y});
 }
 
 /**

src/entities/Camera.cpp
@@ -14,7 +14,6 @@
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-#include "solarus/core/Game.h"
 #include "solarus/core/Map.h"
 #include "solarus/core/System.h"
 #include "solarus/entities/Camera.h"
@@ -25,12 +24,12 @@
 #include "solarus/graphics/Surface.h"
 #include "solarus/graphics/Video.h"
 #include "solarus/lua/LuaContext.h"
-#include "solarus/movements/TargetMovement.h"
+#include "solarus/movements/Movement.h"
 #include "solarus/core/CurrentQuest.h"
 #include "solarus/core/QuestProperties.h"
+#include <iostream>
 
 #include <algorithm>
-#include <list>
 
 namespace Solarus {
 
@@ -940,6 +939,7 @@ Point Camera::get_position_on_screen(Scale px_scale) const {
  */
 void Camera::reset_view() {
   surface->get_view().reset(Rectangle(surface->get_size()));
+  view_applied = false;
 }
 
 /**
@@ -949,11 +949,19 @@ void Camera::apply_view() {
   //TODO add rotation and zoom
   surface->get_view().reset(get_bounding_box());
 
-
   surface->get_view().move(position_offset);
   surface->get_view().zoom(zoom_corr);
   //Just move the view enough to compensate for the rounding
   surface->get_view().rotate(rotation);
+  view_applied = true;
+}
+
+/**
+ * @brief Returns whether the view is set.
+ * @return @c true if the view is applied to the camera surface.
+ */
+bool Camera::is_view_applied() const {
+  return view_applied;
 }
 
 /**