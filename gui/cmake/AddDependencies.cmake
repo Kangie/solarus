b7374fd029d6cdcc1f46d3764c68c02532327eef
Hugo Hromic <hhromic@gmail.com>
2025-02-18 23:59:17 +0000

    Fix finding Qt6LinguistTools when cross-compiling

--- gui/cmake/AddDependencies.cmake
+++ gui/cmake/AddDependencies.cmake
@@ -2,4 +2,16 @@
 
 find_package(Qt6Core "6.8" REQUIRED)
 find_package(Qt6Widgets "6.8" REQUIRED)
-find_package(Qt6LinguistTools "6.8" REQUIRED)
+
+# Find Qt6LinguistTools within the host path when set.
+# This is required for cross compilation with Qt6 as the module is
+# absent in the target install tree.
+find_package(Qt6LinguistTools "6.8" QUIET)
+if (NOT Qt6LinguistTools_FOUND AND QT_HOST_PATH)
+  find_package(Qt6LinguistTools "6.8"
+               PATHS "${QT_HOST_PATH}" "${QT_HOST_PATH}/lib/cmake"
+               NO_CMAKE_FIND_ROOT_PATH NO_DEFAULT_PATH QUIET)
+endif()
+if (NOT Qt6LinguistTools_FOUND)
+  message(FATAL_ERROR "CMake module Qt6LinguistTools could not be found.")
+endif()
