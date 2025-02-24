9d85ad008c52e6b2e3be83e9c03e0ab0f78c38d9
Hugo Hromic <hhromic@gmail.com>
2025-02-24 00:10:09 +0000

    Fix finding Qt6LinguistTools when cross-compiling
    * Also bump minimum required Qt6 version

--- cmake/AddDependencies.cmake
+++ cmake/AddDependencies.cmake
@@ -4,11 +4,24 @@ set(CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH}" "${CMAKE_SOURCE_DIR}/cmake/modules/
 # Wheither LuaJIT should be used instead of vanilla Lua.
 option(SOLARUS_USE_LUAJIT "Use LuaJIT instead of default Lua (recommended)" ON)
 
-find_package(Qt6Core "6.5" REQUIRED)
-find_package(Qt6Widgets "6.5" REQUIRED)
-find_package(Qt6LinguistTools "6.5" REQUIRED)
-find_package(Qt6OpenGL "6.5" REQUIRED)
-find_package(Qt6OpenGLWidgets "6.5" REQUIRED)
+find_package(Qt6Core "6.8" REQUIRED)
+find_package(Qt6Widgets "6.8" REQUIRED)
+find_package(Qt6OpenGL "6.8" REQUIRED)
+find_package(Qt6OpenGLWidgets "6.8" REQUIRED)
+
+# Find Qt6LinguistTools within the host path when set.
+# This is required for cross compilation with Qt6 as the module is
+# absent in the target install tree.
+find_package(Qt6LinguistTools "6.8" QUIET)
+if (NOT Qt6LinguistTools_FOUND AND QT_HOST_PATH)
+  find_package(Qt6LinguistTools "6.8"
+               PATHS "${QT_HOST_PATH}" "${QT_HOST_PATH}/lib/cmake"
+               NO_CMAKE_FIND_ROOT_PATH NO_DEFAULT_PATH QUIET)
+endif()
+if (NOT Qt6LinguistTools_FOUND)
+  message(FATAL_ERROR "CMake module Qt6LinguistTools could not be found.")
+endif()
+
 find_package(SDL2 "2.0.18" REQUIRED)
 find_package(SDL2_image REQUIRED)
 find_package(SDL2_ttf REQUIRED)
