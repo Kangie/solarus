93c7a5f56f8ca681cb2d23f7815fa4a2744e1139
Hugo Hromic <hhromic@gmail.com>
2025-03-04 22:10:49 +0000

    Use correct libmodplug include directives
    * libmodplug headers should be included using the `libmodplug/` prefix
    * Update CMake find module accordingly
    * Example references:
      * https://github.com/libsdl-org/SDL_mixer/blob/main/src/codecs/music_modplug.c
      * https://github.com/icculus/SDL_sound/blob/main/src/SDL_sound_modplug.c
      * https://github.com/FFmpeg/FFmpeg/blob/master/libavformat/libmodplug.c

--- src/audio/ItDecoder.cpp
+++ src/audio/ItDecoder.cpp
@@ -16,8 +16,8 @@
  */
 #include "solarus/audio/ItDecoder.h"
 #include "solarus/core/Debug.h"
-#include <stdafx.h>  // These two headers are with the libmodplug ones.
-#include <sndfile.h>
+#include <libmodplug/stdafx.h>
+#include <libmodplug/sndfile.h>
 
 namespace Solarus {
 
