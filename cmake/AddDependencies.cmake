0ce3a3a27d1939e57bbd631a23e877c9c891f4e7
Hugo Hromic <hhromic@gmail.com>
2025-04-04 23:42:43 +0100

    Exclude Qlementine dependency in CMake from installing
    * Qlementine static lib and includes were installed with the Editor files
    * These files are not required for the Editor to function (static linking)
    * Ref: https://cmake.org/cmake/help/latest/prop_dir/EXCLUDE_FROM_ALL.html

--- cmake/AddDependencies.cmake
+++ cmake/AddDependencies.cmake
@@ -53,5 +53,6 @@ FetchContent_Declare(
   qlementine
   GIT_REPOSITORY https://github.com/oclero/qlementine.git
   GIT_TAG        v1.2.0
+  EXCLUDE_FROM_ALL
 )
 FetchContent_MakeAvailable(qlementine)
