1b6a0bce3512024f982beb578c127e0930678e28
Metallizer <thomas.noury@protonmail.com>
2025-07-24 18:23:07 +0200

    Ensure 1.6 joypad binding names are still supported ("button X")

--- src/core/Joypad.cpp
+++ src/core/Joypad.cpp
@@ -22,6 +22,7 @@ namespace Solarus {
 
 const std::string EnumInfoTraits<JoyPadAxis>::pretty_name = "joypad axis";
 const std::string EnumInfoTraits<JoyPadButton>::pretty_name = "joypad button";
+
 const EnumInfo<JoyPadButton>::names_type EnumInfoTraits<JoyPadButton>::names = {
   {JoyPadButton::A, "a"},
   {JoyPadButton::B, "b"},
@@ -53,6 +54,34 @@ const EnumInfo<JoyPadAxis>::names_type EnumInfoTraits<JoyPadAxis>::names = {
   {JoyPadAxis::RIGHT_Y, "right_y"},
   {JoyPadAxis::TRIGGER_LEFT, "trigger_left"},
   {JoyPadAxis::TRIGGER_RIGHT, "trigger_right"},
+};
+
+std::map<std::string, std::string> Joypad::legacy_bindings_mapping = {
+  {"button 0", "a"},
+  {"button 1", "b"},
+  {"button 2", "x"},
+  {"button 3", "y"},
+  {"button 4", "left_shoulder"},
+  {"button 5", "right_shoulder"},
+  {"button 6", "back"},
+  {"button 7", "start"},
+  {"button 8", "left_stick"},
+  {"button 9", "right_stick"},
+  {"hat 0 up", "dpad_up"},
+  {"hat 0 down", "dpad_down"},
+  {"hat 0 left", "dpad_left"},
+  {"hat 0 right", "dpad_right"},
+
+  {"axis 0 -", "left_x -"},
+  {"axis 0 +", "left_x +"},
+  {"axis 1 -", "left_y -"},
+  {"axis 1 +", "left_y +"},
+  {"axis 3 -", "right_x -"},
+  {"axis 3 +", "right_x +"},
+  {"axis 4 -", "right_y -"},
+  {"axis 4 +", "right_y +"},
+  {"axis 2 -", "trigger_left"},
+  {"axis 5 -", "trigger_right"},
 };
 
 Joypad::Joypad(SDL_GameController *sdl_gc, SDL_Joystick *sdl_js) :
