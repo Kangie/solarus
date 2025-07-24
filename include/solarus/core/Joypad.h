40b5d8922b63d79242468d38362a836905c6c904
Metallizer <thomas.noury@protonmail.com>
2025-07-24 19:19:42 +0200

    Fixed extra qualification on legacy bindings for Joypad class

--- include/solarus/core/Joypad.h
+++ include/solarus/core/Joypad.h
@@ -84,7 +84,7 @@ class Joypad final : public ExportableToLua
 public:
   // static information
   static constexpr const char module_name[] = "sol.joypad";
-  static std::map<std::string, std::string> Joypad::legacy_bindings_mapping;
+  static std::map<std::string, std::string> legacy_bindings_mapping;
 
   Joypad(SDL_GameController* sdl_gc, SDL_Joystick* sdl_js);
   double get_axis(JoyPadAxis axis) const;
