30123c1fc863f3b764f56146314f5a0becf3e63c
S2009 <contact@s2009.fr>
2025-12-27 19:17:54 +0100

    fix(docs): :bug: igen arg types specific case

--- doc/igen.py
+++ doc/igen.py
@@ -214,7 +214,7 @@ def generate_args(section):
             elif detail.startswith("requires: "):
                 arg_requirements = detail.replace("requires: ", "").replace("`", "").split(" and ")
             else:
-                arg_types = re.sub(r'\]\(.*?\)', "", detail.replace("[", "")).replace("`", "").replace("map entity", "entity").split(" or ")
+                arg_types = re.sub(r'\]\(.*?\)', "", detail.replace("[", "")).replace("`", "").replace("map entity", "entity").replace("carried object", "carried_object").split(" or ")
 
         args.append({"name": arg_name, "desc": arg_desc, "optionnal": optionnal_arg, "deprecated": deprecated_arg, "requires": arg_requirements, "types": arg_types, "default": arg_default_value, "values": generate_values(arg_section), "properties": generate_properties(arg_section)})
 
